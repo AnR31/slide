124cebecfe3a24b6d6ed226521372b517a1580eb
Roman K <roman.k.person@gmail.com>
2020-09-05 11:29:03 +0300

    +component

---
+++ src/app/mybtn/mybtn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mybtn',
+  templateUrl: './mybtn.component.html',
+  styleUrls: ['./mybtn.component.css']
+})
+export class MybtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
